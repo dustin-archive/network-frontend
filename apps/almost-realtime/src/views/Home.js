375f7f804189b8817a738506164de55ac1afa4f0
Dustin Dowell <dustindowell22@gmail.com>
2019-02-09 02:32:57 -0600

    separate chat and home

--- apps/almost-realtime/src/views/Home.js
+++ apps/almost-realtime/src/views/Home.js
@@ -1,78 +1,11 @@
 
-import { div, h1, input } from '@hyperapp/html'
-
-const MessageInput = () => (state, actions) => {
-  const onkeydown = e => {
-    if (e.key === 'Enter') {
-      actions.postComment({
-        clientID: state.clientID,
-        comment: {
-          message: e.target.value,
-          name: state.clientName
-        }
-      })
-
-      e.target.value = ''
-    }
-  }
-
-  return input({ placeholder: 'Message', onkeydown, type: 'text' })
-}
-
-const CommentList = () => (state, actions) => {
-  const { commentList } = state
-  const result = []
-
-  for (let i = 0; i < commentList.length; i++) {
-    const { message, name } = commentList[i]
-    result.push(div(name + ': ' + message))
-  }
-
-  const onupdate = el => {
-    el.scrollTop = el.scrollHeight - el.clientHeight
-  }
-
-  return div({ class: 'home-comment', onupdate }, result)
-}
-
-const NameInput = () => (state, actions) => {
-  const onkeyup = e => {
-    actions.update({
-      clientName: e.target.value
-    })
-  }
-
-  return input({ placeholder: 'Name', onkeyup, type: 'text' })
-}
-
-const ClientList = () => state => {
-  const clientList = state.clientList
-  const result = []
-
-  for (let i = 0; i < clientList.length; i++) {
-    const client = clientList[i]
-
-    result.push(
-      div(client.name || client.id)
-    )
-  }
-
-  return div({ class: 'home-client' }, result)
-}
+import { div, h1 } from '@hyperapp/html'
+import Chat from './Chat'
 
 const Home = () => (state, actions) => {
   return div({ class: 'home' }, [
     h1('Almost Realtime'),
-    div({ class: 'home-main' }, [
-      div([
-        ClientList,
-        NameInput
-      ]),
-      div([
-        CommentList,
-        MessageInput
-      ])
-    ])
+    Chat
   ])
 }
 
